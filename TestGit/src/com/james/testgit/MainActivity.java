86e8be4994aca2df56350b0ba656ec11e32aa570
xujinyang <1119117546@qq.com>
2014-03-11 22:43:22 +0800

    add to branch

--- TestGit/src/com/james/testgit/MainActivity.java
+++ TestGit/src/com/james/testgit/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         init();
         setListener();
-        
     }
 
 
---
+++ TestGit/src/com/james/testgit/branch.java
@@ -0,0 +1,6 @@
+package com.james.testgit;
+
+public class branch {
+	private int branch1=1;
+
+}
